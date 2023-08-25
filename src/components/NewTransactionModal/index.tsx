2bace01c2b262b397f36b088df12bb926e6b1464
Laura Andrade <lauraandradecamp@gmail.com>
2023-08-24 21:58:24 -0300

    installing serverJson

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { Content, Overlay } from './styles';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import * as RadioGroup from '@radix-ui/react-radio-group';
 
 export function NewTransactionModal() {
   return(
@@ -9,17 +11,30 @@ export function NewTransactionModal() {
       <Content>
         <Dialog.Title>Nova transação</Dialog.Title>
 
+        <CloseButton> 
+          <X size={24} />
+        </CloseButton>
+
         <form action=''>
           <input type="text" placeholder='Descrição' required />
           <input type="number" placeholder='Preço' required />
           <input type="text" placeholder='Categoria' required />
 
-          <button type='submit'>
+          <TransactionType>
+            <TransactionTypeButton variant='income' value='income'>
+              <ArrowCircleUp size={24}/>
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant='outcome' value='outcome'>
+              <ArrowCircleDown size={24}/>
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+          <button type="submit">
             Cadastrar
           </button>
         </form>
-
-        <Dialog.Close />
       </Content>
     </Dialog.Portal>
   )
