ab02b18a28e9fbd9d13e335d72db7dc31072f0d3
Laura Andrade <lauraandradecamp@gmail.com>
2023-08-25 21:35:35 -0300

    creating contexts

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,12 @@
 import { SummaryContainer, SummaryCard } from "./styles";
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
+import { useContext } from 'react';
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  
   return(
     <SummaryContainer>
       <SummaryCard>
